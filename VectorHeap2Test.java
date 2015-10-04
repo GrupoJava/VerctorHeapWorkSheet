65e3d9a4e7f693f620ab407058b915e0da41388c
Stardock <josecorona1460@gmail.com>
2015-10-03 21:53:43 -0600

    v5

---
+++ VectorHeap2Test.java
@@ -0,0 +1,65 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.Assert.*;
+import java.util.Vector;
+import junit.framework.TestCase;
+// TODO: Auto-generated Javadoc
+
+/**
+ * The Class VectorHeapTest.
+ *
+ * @author Admin
+ */
+
+public class VectorHeap2Test extends TestCase{
+	    
+    	/**
+    	 * Instantiates a new vector heap test.
+    	 */
+    	public VectorHeap2Test(){
+	    }
+	    
+	    /** The heap. */
+    	VectorHeap2<String> heap = new VectorHeap2();
+	    
+	    /**
+    	 * Test add.
+    	 */
+    	@Test
+	    public void testAdd(){
+	        System.out.println("add");
+	        String prioridad = "A";
+	        heap.add(prioridad);
+	        assertEquals(prioridad,heap.remove());
+	    }
+	    
+	    /**
+    	 * Test remove.
+    	 */
+    	@Test
+	    public void testRemove(){
+	        System.out.println("remove");
+	        
+	        String prioridad1="A";
+	        String prioridad2="B";
+	        heap.add(prioridad1);
+	        heap.add(prioridad2);
+	        String Result = "B";
+	        String result = heap.remove();  // "A"
+	        result = heap.remove();         // "B"
+	        assertEquals(Result, result);   ///"B"="B"
+	    }
+	        
+	    }
+
